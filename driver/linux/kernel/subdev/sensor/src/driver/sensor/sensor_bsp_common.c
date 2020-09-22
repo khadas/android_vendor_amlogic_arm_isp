khadas/android_vendor_amlogic_common_arm_isp

isp: isp driver sync with latest internal version [1/2]
PD#SWPL-19892

Problem:
Android ISP driver upgrade

Solution:
Android ISP Driver upgrade

Verify:
Local

Change-Id: Ia8e038d86245d164bfcaae5ae03c5f26cee4747b
Signed-off-by: xiaoxin.cao <xiaoxin.cao@amlogic.com>

driver/linux/kernel/subdev/sensor/src/driver/sensor/sensor_bsp_common.c
@@ -35,7 +35,6 @@ int pwr_am_enable(sensor_bringup_t* sensor_bp, const char* propname, int val)
 		devm_gpio_request(sensor_bp->dev, sensor_bp->vana, "POWER");
 		if (gpio_is_valid(sensor_bp->vana)) {
 			gpio_direction_output(sensor_bp->vana, val);
-			pr_info("pwr_enable: power gpio init\n");
 		} else {
 			pr_err("pwr_enable: gpio %s is not valid\n", propname);
 			return -1;
@@ -68,7 +67,6 @@ int pwr_ir_cut_enable(sensor_bringup_t* sensor_bp, int propname, int val)
 		devm_gpio_request(sensor_bp->dev, propname, "POWER");
 		if (gpio_is_valid(propname)) {
 			gpio_direction_output(propname, val);
-			pr_info("pwr_enable: power gpio init\n");
 		} else {
 			pr_err("pwr_enable: gpio %d is not valid\n", propname);
 			return -1;
@@ -92,7 +90,6 @@ int reset_am_enable(sensor_bringup_t* sensor_bp, const char* propname, int val)
 		devm_gpio_request(sensor_bp->dev, sensor_bp->reset, "RESET");
 		if (gpio_is_valid(sensor_bp->reset)) {
 			gpio_direction_output(sensor_bp->reset, val);
-			pr_info("reset init\n");
 		} else {
 			pr_err("reset_enable: gpio %s is not valid\n", propname);
 			return -1;
@@ -129,7 +126,7 @@ int clk_am_enable(sensor_bringup_t* sensor_bp, const char* propname)
 
 	clk_prepare_enable(clk);
 	clk_val = clk_get_rate(clk);
-	pr_info("isp init clock is %d MHZ\n",clk_val/1000000);
+	pr_info("init mclock is %d MHZ\n",clk_val/1000000);
 
 	sensor_bp->mclk = clk;
 	return 0;
@@ -154,7 +151,7 @@ int gp_pl_am_enable(sensor_bringup_t* sensor_bp, const char* propname, uint32_t 
 	if (ret < 0)
 		pr_err(" clk_prepare_enable failed\n");
 	clk_val = clk_get_rate(clk);
-	pr_err("isp init clock is %d MHZ\n",clk_val/1000000);
+	pr_err("init mclock is %d MHZ\n",clk_val/1000000);
 
 	sensor_bp->mclk = clk;
 	return 0;

khadas/android_vendor_amlogic_arm_isp

driver/linux/kernel/subdev/sensor/src/driver/sensor/sensor_bsp_common.c
@@ -47,7 +47,6 @@ int pwr_am_enable(sensor_bringup_t* sensor_bp, const char* propname, int val)
 		devm_gpio_request(sensor_bp->dev, sensor_bp->vana, "POWER");
 		if (gpio_is_valid(sensor_bp->vana)) {
 			gpio_direction_output(sensor_bp->vana, val);
-			pr_info("pwr_enable: power gpio init\n");
 		} else {
 			pr_err("pwr_enable: gpio %s is not valid\n", propname);
 			return -1;
@@ -90,7 +89,6 @@ int reset_am_enable(sensor_bringup_t* sensor_bp, const char* propname, int val)
 	ret = sensor_bp->reset;
 
 	if (ret >= 0) {
-		pr_info("reset init\n");
 		gpio_expander_direction_output(3, 0);
 		mdelay(20);
 		gpio_expander_direction_output(3, 1);
@@ -127,7 +125,7 @@ int clk_am_enable(sensor_bringup_t* sensor_bp, const char* propname)
 	clk_set_rate(clk, 24000000);
 	clk_prepare_enable(clk);
 	clk_val = clk_get_rate(clk);
-	pr_info("isp init clock is %d MHZ\n",clk_val/1000000);
+	pr_info("init clock is %d MHZ\n",clk_val/1000000);
 
 	sensor_bp->mclk = clk;
 	return 0;
@@ -152,7 +150,7 @@ int gp_pl_am_enable(sensor_bringup_t* sensor_bp, const char* propname, uint32_t 
 	if (ret < 0)
 		pr_err(" clk_prepare_enable failed\n");
 	clk_val = clk_get_rate(clk);
-	pr_err("isp init clock is %d MHZ\n",clk_val/1000000);
+	pr_err("init clock is %d MHZ\n",clk_val/1000000);
 
 	sensor_bp->mclk = clk;
 	return 0;