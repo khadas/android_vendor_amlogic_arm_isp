931fe14d3f2eb9218c4eefb37d1a568529fd680b
Xiong Zhang <xiong.zhang@wesion.com>
2023-02-11 20:53:01 +0800

    Camera: Compatible with both OS08A10 and IMX415 [3/3]
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- driver/linux/kernel/subdev/sensor/app/soc_sensor.c
+++ driver/linux/kernel/subdev/sensor/app/soc_sensor.c
@@ -617,7 +617,7 @@ static int32_t soc_sensor_probe( struct platform_device *pdev )
         LOG(LOG_ERR, "Failed to alloc mem\n");
         return -ENOMEM;
     }
-    rtn = of_property_read_string(dev->of_node, "sensor-name", &sensor_name);
+    rtn = of_property_read_string(dev->of_node, "sensor_name", &sensor_name);
 
     if (rtn != 0) {
         pr_err("%s: failed to get sensor name\n", __func__);
